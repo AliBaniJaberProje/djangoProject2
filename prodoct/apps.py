bcbaec0581cc07ef50e45c659c444b5025acf65e
AliBaniJaberProje <alibanijaber1999@gmail.com>
2021-04-05 04:27:11 +0300

    Initial commit

---
+++ prodoct/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProdoctConfig(AppConfig):
+    name = 'prodoct'
---
+++ prodoct/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from prodoct import views
+
+urlpatterns = [
+    path('jaber/',views.home),
+
+]
